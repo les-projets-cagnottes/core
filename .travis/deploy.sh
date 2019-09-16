76779560a81b2daaa4189a935234aa8f514bf3a4
Thomas HINGANT <5090230+Thomah@users.noreply.github.com>
2019-09-16 18:41:05 +0200

    Automatic deploy with Travis (#26)
    * Fix project editing (#25)
    
    * Automatic deploy with Travis

---
+++ .travis/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ! [[ "$TRAVIS_BRANCH" == "master" ]]; then exit 0; fi
+
+eval "$(ssh-agent -s)"
+chmod 600 $TRAVIS_BUILD_DIR/.travis/id_rsa
+ssh-add $TRAVIS_BUILD_DIR/.travis/id_rsa
+ssh-keyscan -t rsa -H $IP >> ~/.ssh/known_hosts
+
+ssh -p $PORT apps@$IP -o StrictHostKeyChecking=no "$( cat <<EOT
+    sudo service valyou stop
+    exit
+EOT
+)"
+
+scp $TRAVIS_BUILD_DIR/bin/valyou.service apps@$IP:$DEPLOY_DIR
+scp $TRAVIS_BUILD_DIR/bin/setenv.sh.template apps@$IP:$DEPLOY_DIR
+scp $TRAVIS_BUILD_DIR/bin/valyou.sh apps@$IP:$DEPLOY_DIR
+scp $TRAVIS_BUILD_DIR/target/valyou-*.jar apps@$IP:$DEPLOY_DIR/valyou-api.jar
+
+ssh -p $PORT apps@$IP -o StrictHostKeyChecking=no "$( cat <<EOT
+    cd $DEPLOY_DIR
+    sudo mv valyou.service /etc/systemd/system/valyou.service
+    sudo systemctl daemon-reload
+    echo "$(date -u) Travis Deploy"  >> ./logs/valyou-api.log
+    sudo service valyou start
+    exit
+EOT
+)"
+
+rm $TRAVIS_BUILD_DIR/.travis/id_rsa
