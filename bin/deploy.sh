79a5a3cad62ae03d1a12f9d1dad3084d269cbbfa
Thomah <5090230+Thomah@users.noreply.github.com>
2020-03-07 20:56:56 +0100

    Update scp action

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,10 +10,10 @@ sudo service les-projets-cagnottes-core stop
 sleep 1
 
 mkdir -p ${FINAL_LOCATION}
-sudo cp ${SCRIPTPATH}/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
-cp ${SCRIPTPATH}/bin/setenv.sh.template ${FINAL_LOCATION}/setenv.template
-cp ${SCRIPTPATH}/bin/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/les-projets-cagnottes-core.sh
-cp ${SCRIPTPATH}/target/core-${TAG_NAME}.jar ${FINAL_LOCATION}/les-projets-cagnottes-core.jar
+sudo cp ${TMP_LOCATION}/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
+cp ${TMP_LOCATION}/setenv.sh.template ${FINAL_LOCATION}/setenv.template
+cp ${TMP_LOCATION}/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/les-projets-cagnottes-core.sh
+cp ${TMP_LOCATION}/core-${TAG_NAME}.jar ${FINAL_LOCATION}/les-projets-cagnottes-core.jar
 
 sudo systemctl daemon-reload
 sudo service les-projets-cagnottes-core start
