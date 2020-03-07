f68e780bd194a47737f8065e5d7c7fbe94aa4556
Thomah <5090230+Thomah@users.noreply.github.com>
2020-03-07 21:41:21 +0100

    Move scp target

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TAG_NAME=$1
-TMP_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"/..
+DEPLOY_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"
 FINAL_LOCATION=/opt/les-projets-cagnottes/core
 
 echo "$(date -u) Automatic Deploy"  >> ./console.log
@@ -9,11 +9,12 @@ echo "$(date -u) Automatic Deploy"  >> ./console.log
 sudo service les-projets-cagnottes-core stop
 sleep 1
 
-mkdir -p ${FINAL_LOCATION}
-sudo cp ${TMP_LOCATION}/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
-cp ${TMP_LOCATION}/setenv.sh.template ${FINAL_LOCATION}/setenv.template
-cp ${TMP_LOCATION}/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/les-projets-cagnottes-core.sh
-cp ${TMP_LOCATION}/core-${TAG_NAME}.jar ${FINAL_LOCATION}/les-projets-cagnottes-core.jar
+mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
+sudo cp ${DEPLOY_LOCATION}/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
+cp ${DEPLOY_LOCATION}/setenv.sh.template ${FINAL_LOCATION}/setenv.template
+cp ${DEPLOY_LOCATION}/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/les-projets-cagnottes-core.sh
+cp ${DEPLOY_LOCATION}/core-${TAG_NAME}.jar ${FINAL_LOCATION}/les-projets-cagnottes-core.jar
+cp -R ${DEPLOY_LOCATION} ${FINAL_LOCATION}/${TAG_NAME}
 
 sudo systemctl daemon-reload
 sudo service les-projets-cagnottes-core start
