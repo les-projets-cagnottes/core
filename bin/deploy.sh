a3373530f681a2492d806ec11d5021093422b991
Thomah <5090230+Thomah@users.noreply.github.com>
2020-03-08 14:31:54 +0100

    Fix deploy location in release process

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TAG_NAME=$1
-DEPLOY_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"
+DEPLOY_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"/..
 FINAL_LOCATION=/opt/les-projets-cagnottes/core
 
 echo "$(date -u) Automatic Deploy"  >> ./console.log
@@ -10,10 +10,10 @@ sudo service les-projets-cagnottes-core stop
 sleep 1
 
 mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
-sudo cp ${DEPLOY_LOCATION}/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
-cp ${DEPLOY_LOCATION}/setenv.sh.template ${FINAL_LOCATION}/setenv.template
-cp ${DEPLOY_LOCATION}/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/les-projets-cagnottes-core.sh
-cp ${DEPLOY_LOCATION}/core-${TAG_NAME}.jar ${FINAL_LOCATION}/les-projets-cagnottes-core.jar
+sudo cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
+cp ${DEPLOY_LOCATION}/bin/setenv.sh.template ${FINAL_LOCATION}/setenv.template
+cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/les-projets-cagnottes-core.sh
+cp ${DEPLOY_LOCATION}/target/core-${TAG_NAME}.jar ${FINAL_LOCATION}/les-projets-cagnottes-core.jar
 cp -R ${DEPLOY_LOCATION} ${FINAL_LOCATION}/${TAG_NAME}
 
 sudo systemctl daemon-reload
