70070e7162803f0aa4889ec47539821c38c84b77
Thomah <5090230+Thomah@users.noreply.github.com>
2020-07-04 17:19:52 +0200

    #78 Fix production deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 TAG_NAME=$1
-DEPLOY_LOCATION="$( cd "$(dirname "$0")" ; pwd -P )"/..
+DEPLOY_LOCATION="$( cd "$(dirname "$0")" || exit ; pwd -P )"/..
 FINAL_LOCATION=/opt/les-projets-cagnottes/core
+FINAL_APP_LOCATION=/opt/les-projets-cagnottes/core/${TAG_NAME}
 
 echo "$(date -u) Automatic Deploy"  >> ./console.log
 
@@ -10,23 +11,26 @@ sudo service les-projets-cagnottes-core stop
 sleep 1
 
 # Backup environment config
-cp ${FINAL_LOCATION}/current/setenv.sh ${DEPLOY_LOCATION}/setenv.sh
-cp ${FINAL_LOCATION}/current/application.properties ${DEPLOY_LOCATION}/application.properties
+cp "${FINAL_LOCATION}/current/setenv.sh" "${DEPLOY_LOCATION}/setenv.sh"
+cp "${FINAL_LOCATION}/current/application.properties" "${DEPLOY_LOCATION}/application.properties"
 
 # Update systemd
-sudo cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
+sudo cp "${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.service" /etc/systemd/system/les-projets-cagnottes-core.service
 
 # Create folder for version
-mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
-cp ${DEPLOY_LOCATION}/bin/setenv.sh.template ${FINAL_LOCATION}/${TAG_NAME}/setenv.template
-cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/${TAG_NAME}/les-projets-cagnottes-core.sh
-cp ${DEPLOY_LOCATION}/target/core-${TAG_NAME}.jar ${FINAL_LOCATION}/${TAG_NAME}/les-projets-cagnottes-core.jar
-cp ${DEPLOY_LOCATION}/setenv.sh ${FINAL_LOCATION}/${TAG_NAME}/setenv.sh
-cp ${DEPLOY_LOCATION}/application.properties ${FINAL_LOCATION}/${TAG_NAME}/application.properties
+mkdir -p "${FINAL_APP_LOCATION}"
+cp "${DEPLOY_LOCATION}/bin/setenv.sh.template" "${FINAL_APP_LOCATION}/setenv.template"
+cp "${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.sh" "${FINAL_APP_LOCATION}/les-projets-cagnottes-core.sh"
+cp "${DEPLOY_LOCATION}/target/core-${TAG_NAME}.jar" "${FINAL_APP_LOCATION}/les-projets-cagnottes-core.jar"
+cp "${DEPLOY_LOCATION}/setenv.sh" "${FINAL_APP_LOCATION}/setenv.sh"
+cp "${DEPLOY_LOCATION}/application.properties" "${FINAL_APP_LOCATION}/application.properties"
 
 # Update current symlink
-ln -sf ${FINAL_LOCATION}/${TAG_NAME} ${FINAL_LOCATION}/current
+cd "${FINAL_LOCATION}" || exit
+rm current
+ln -s "${TAG_NAME}" current
 
+# Restart service
 sudo systemctl daemon-reload
 sudo service les-projets-cagnottes-core start
 
