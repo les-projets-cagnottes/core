b5089734e5671b3416bdfa6629f6027c0e466e62
Thomah <5090230+Thomah@users.noreply.github.com>
2020-07-04 16:17:50 +0200

    #77 #78 Fix pre-release and update deploy in production workflow

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,19 +9,23 @@ echo "$(date -u) Automatic Deploy"  >> ./console.log
 sudo service les-projets-cagnottes-core stop
 sleep 1
 
+# Backup environment config
 cp ${FINAL_LOCATION}/current/setenv.sh ${DEPLOY_LOCATION}/setenv.sh
 cp ${FINAL_LOCATION}/current/application.properties ${DEPLOY_LOCATION}/application.properties
 
-mkdir -p ${FINAL_LOCATION}/current
+# Update systemd
 sudo cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.service /etc/systemd/system/les-projets-cagnottes-core.service
-cp ${DEPLOY_LOCATION}/bin/setenv.sh.template ${FINAL_LOCATION}/current/setenv.template
-cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/current/les-projets-cagnottes-core.sh
-cp ${DEPLOY_LOCATION}/target/core-${TAG_NAME}.jar ${FINAL_LOCATION}/current/les-projets-cagnottes-core.jar
-cp ${DEPLOY_LOCATION}/setenv.sh ${FINAL_LOCATION}/current/setenv.sh
-cp ${DEPLOY_LOCATION}/application.properties ${FINAL_LOCATION}/current/application.properties
 
+# Create folder for version
 mkdir -p ${FINAL_LOCATION}/${TAG_NAME}
-cp -R ${DEPLOY_LOCATION} ${FINAL_LOCATION}/${TAG_NAME}
+cp ${DEPLOY_LOCATION}/bin/setenv.sh.template ${FINAL_LOCATION}/${TAG_NAME}/setenv.template
+cp ${DEPLOY_LOCATION}/bin/les-projets-cagnottes-core.sh ${FINAL_LOCATION}/${TAG_NAME}/les-projets-cagnottes-core.sh
+cp ${DEPLOY_LOCATION}/target/core-${TAG_NAME}.jar ${FINAL_LOCATION}/${TAG_NAME}/les-projets-cagnottes-core.jar
+cp ${DEPLOY_LOCATION}/setenv.sh ${FINAL_LOCATION}/${TAG_NAME}/setenv.sh
+cp ${DEPLOY_LOCATION}/application.properties ${FINAL_LOCATION}/${TAG_NAME}/application.properties
+
+# Update current symlink
+ln -sf ${FINAL_LOCATION}/${TAG_NAME} ${FINAL_LOCATION}/current
 
 sudo systemctl daemon-reload
 sudo service les-projets-cagnottes-core start
