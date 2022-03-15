23745b8451c8ba92dcc8af43bf1d6e0fd5bc59c5
jeremylegendre44 <jeremy.legendre44@gmail.com>
2022-03-15 17:47:32 +0100

    Delete condition isEmpty on publicationChannelId this field can equals to null or empty string

--- src/main/java/fr/lesprojetscagnottes/core/slack/controller/SlackTeamController.java
+++ src/main/java/fr/lesprojetscagnottes/core/slack/controller/SlackTeamController.java
@@ -84,7 +84,7 @@ public class SlackTeamController {
 
         // Verify that ID is correct
         // Fails if any of references are null
-        if (slackTeamModel == null || StringUtils.isEmpty(slackTeamModel.getTeamId()) || StringUtils.isEmpty(slackTeamModel.getPublicationChannelId())) {
+        if (slackTeamModel == null || StringUtils.isEmpty(slackTeamModel.getTeamId())) {
             if (slackTeamModel != null) {
                 log.error("Impossible to update project {} : some references are missing", slackTeamModel.getId());
             } else {
