7060c0a1a248ff8fc6ff2615698227a232363a24
Thomah <5090230+Thomah@users.noreply.github.com>
2023-04-03 15:20:05 +0200

    fix: change column 'variables' of notifications to text type

--- src/main/resources/db/changelog/db.changelog-0.16.sql
+++ src/main/resources/db/changelog/db.changelog-0.16.sql
@@ -5,3 +5,7 @@ ALTER TABLE projects
     ADD last_status_update timestamp without time zone DEFAULT now();
 UPDATE projects SET last_status_update = updated_at;
 --rollback alter table projects drop column last_status_update;
+
+--changeset lesprojetscagnottes:change-notifications-variables-type
+ALTER TABLE notifications ALTER COLUMN variables TYPE text;
+--rollback alter table notifications alter column variables type varchar(255);
